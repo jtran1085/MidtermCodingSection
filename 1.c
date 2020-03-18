c8bba1d929475e6ef76d94226ff0d98de0854fea
jtran1085 <60716832+jtran1085@users.noreply.github.com>
2020-03-18 15:50:08 -0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,13 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 void count() {
- printf("Enter lines of text here: .../n");
- int c,nl, nc, nt, nbackslash = 0;
- while ((c = fgetc(stdin)) != EOF) {
+ printf("Enter lines of text here:\n");
+ int c=0;
+ int nl=0;
+ int nc=0;
+ int nt=0;
+ int nbackslash = 0;
+
+ while ((c = fgetc(stdin)) != EOF && c!= '\0') {
  if (c == '\n') { ++nl;
- } else if (c == '\t') { ++nt;
- } else if (c == '\\') { ++nbackslash;
- } else if (isspace(c)) { ++nc; }
- fprintf("lines: %d\n", nl);
- fprintf("tabs: %s\n", nt);
- fprintf("backslashes: %u\n", nbackslash);
- fprintf("alphanumeric: %f\n", nc);
+ } if (c == '\t') { ++nt;
+ } if (c == '\\') { ++nbackslash;
+ } if (isalnum(c)) { ++nc; }
+ printf("lines: %d\n", nl);
+ printf("tabs: %d\n", nt);
+ printf("backslashes: %d\n", nbackslash);
+ printf("alphanumeric: %d\n", nc);
+  }
 }
+int main(int argc, const char* argv[]) {
+ count();
+ return 0;
+} 
